6a3f4a7ed19f820bb990cdb04fa2c329224f9dc8
OscarCrDV <105087839+OscarCrDV@users.noreply.github.com>
2026-01-23 10:39:49 -0500

    Update main.py
    Oscar hizo esto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from fincas import router as fincas_router
 from sectores import router as sectores_router
@@ -10,18 +11,34 @@ from auth_simple import require_api_key
 
 app = FastAPI(dependencies=[Depends(require_api_key)])
 
+# ------------------------
+# CORS
+# ------------------------
+origins = [
+    "http://localhost:5173",          # frontend local
+    "https://elcolibriapp.vercel.app" # frontend en producción
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # o ["*"] para pruebas
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ------------------------
 # REGISTRAR ROUTERS
+# ------------------------
 app.include_router(fincas_router)
 app.include_router(sectores_router)
 app.include_router(trabajadores_router)
 app.include_router(revisiones_router)
 app.include_router(usuarios_router)
 
-
+# ------------------------
+# RUTA RAÍZ
+# ------------------------
 @app.get("/")
 def root():
     return {"status": "ok"}
-
-
-
-
