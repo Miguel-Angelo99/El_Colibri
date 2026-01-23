e0839780f49d9b9afda052246ee42c3a10737b09
Miguel-Angelo99 <loayzamiguel98@gmail.com>
2026-01-23 01:30:23 -0500

    Refactor FastAPI app initialization and imports
    Removed duplicate FastAPI initialization and restructured the import statements for clarity.

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from fastapi import FastAPI
+
 from fincas import router as fincas_router
 from sectores import router as sectores_router
-from revisiones import router as revisiones_router
 from trabajadores import router as trabajadores_router
-from fastapi import FastAPI, Depends
-from auth_simple import require_api_key
+from revisiones import router as revisiones_router
 
-app = FastAPI(title="El Colibrí API")
+app = FastAPI(title="El Colibri API")
 
+# REGISTRAR ROUTERS
 app.include_router(fincas_router)
 app.include_router(sectores_router)
-app.include_router(revisiones_router)
 app.include_router(trabajadores_router)
-app = FastAPI(dependencies=[Depends(require_api_key)])
+app.include_router(revisiones_router)
+
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
 
 
